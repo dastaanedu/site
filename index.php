05ac01b88a2c0eee3ca7043a59ca4e4abeac0291
dastaanedu <142664283+dastaanedu@users.noreply.github.com>
2023-08-23 18:04:57 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DastaanEdu - Learn, Lead, Liberate</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" href="Logo.png">
+</head>
+<body>
+    <header>
+        <h1>Welcome to DastaanEdu</h1>
+        <p>Your Destination for Learning and Exploration</p>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="courses.php">Courses</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section id="home">
+            <h2>Learn and Explore with DastaanEdu</h2>
+            <p>Discover a world of knowledge and inspiration through our interactive courses and resources.</p>
+        </section>
+
+        <section id="mission">
+            <h2>Mission Statement</h2>
+            <p>"Elevating Education for All: At DastaanEdu, our mission is to bridge the gap in education by providing accessible and high-quality learning opportunities to underserved Pakistani students. Through our online platform, we aim to empower these students with knowledge, skills, and opportunities they might not otherwise have access to. By nurturing their potential, we are dedicated to shaping a brighter future for every young mind in Pakistan."</p>
+        </section>
+        <section id="objectives">
+            <h2>Objectives</h2>
+            <ul class="objective-list">
+                <li>Ensure equal access to education for all Pakistani children, regardless of their background.</li>
+                <li>Develop and curate interactive educational content that addresses the needs of underserved students.</li>
+                <li>Foster a culture of lifelong learning by offering diverse courses for different interests and abilities.</li>
+                <li>Equip students with essential life skills and critical thinking abilities.</li>
+                <li>Celebrate Pakistan's cultural heritage by integrating local stories and traditions into our content.</li>
+                <li>Collaborate with communities, parents, and educators to create a holistic learning experience.</li>
+                <li>Empower educators with resources and training to enhance the learning experience.</li>
+                <li>Monitor and evaluate the impact of our initiatives on students' learning outcomes.</li>
+                <li>Establish partnerships with NGOs, educational institutions, and government bodies to expand our reach.</li>
+                <li>Work towards the sustainability of DastaanEdu through strategic planning and fundraising.</li>
+                <li>Advocate for improved technology accessibility in underserved areas.</li>
+                <li>Embrace technological advancements and pedagogical innovations.</li>
+            </ul>
+        </section>
+        
+    </main>
+
+    <section id="word">
+        <h2>Word From the Founder</h2>
+        <p>Dear All,<br>I'm Arshian Ali, founder of DastaanEdu. As a Pashtun, I've witnessed the educational neglect faced by fellow Pakistanis in remote regions. DastaanEdu is my response—a commitment to bring quality education to every Pakistani child. Our platform breaks barriers and offers a chance for those who need it most. With your support, we can make dreams real for these young minds, fostering a future where every child has the opportunity to learn, grow, and thrive.<br>Arshian Ali</p>
+    </section>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> DastaanEdu. All rights reserved.</p>
+    </footer>
+</body>
+</html>
